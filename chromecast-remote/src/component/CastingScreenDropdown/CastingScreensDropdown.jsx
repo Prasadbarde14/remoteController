7a1051be157daf7a0d367ae98859a781ee91d9a6
gaian <prasad.barde@gaiansolutions.com>
2024-04-08 16:02:32 +0530

    remote controller for CEO room video wall

---
+++ chromecast-remote/src/component/CastingScreenDropdown/CastingScreensDropdown.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "./castingscreendropdown.css";
+
+const CastingScreensDropdown = ({ tvNumber }) => {
+  const websiteUrls = [
+    "https://edo.aidtaas.com/",
+    "https://izak.aidtaas.com",
+   " https://museo.aidtaas.com",
+   " https://around.aidtaas.com",
+   " https://gofema.aidtaas.com",
+    "https://amplyfund.aidtaas.com",
+    "https://nexusconnects.ai/",
+  ];
+
+  const [selectedUrls, setSelectedUrls] = useState(() => {
+    const storedUrls = localStorage.getItem(`tv${tvNumber}`);
+    return storedUrls ? JSON.parse(storedUrls) : [];
+  });
+
+  const handleCheckboxChange = (url) => {
+    setSelectedUrls((prevSelected) =>
+      prevSelected.includes(url)
+        ? prevSelected.filter((selectedUrl) => selectedUrl !== url)
+        : [...prevSelected, url]
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem(`tv${tvNumber}`, JSON.stringify(selectedUrls));
+  }, [selectedUrls, tvNumber]);
+
+  return (
+    <div className="castingscreen-dropdown">
+      {websiteUrls.map((url) => (
+        <li key={url} >
+          <input
+            type="checkbox"
+            className="urlcheckbox"
+            id={url}
+            checked={selectedUrls.includes(url)}
+            onChange={() => handleCheckboxChange(url)}
+          />
+          <label htmlFor={url}>{url}</label>
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default CastingScreensDropdown;
+
